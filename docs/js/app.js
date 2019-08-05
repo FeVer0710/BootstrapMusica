55b5b182cbd4119308e00b42d76b2535d9168039
Jaime Berríos <ingjaime1999@gmail.com>
2019-08-05 17:44:06 -0600

    Todo por hoy 08/05/19

---
+++ docs/js/app.js
@@ -0,0 +1,53 @@
+
+
+
+
+// Smooth Scroll con js nativo
+document.querySelectorAll('.navbar .nav-link').forEach(enlace=>{
+	enlace.addEventListener('click',(e)=>{
+		e.preventDefault();
+
+		document.querySelector(enlace.getAttribute('href')).scrollIntoView({
+
+
+			behavior:'smooth'
+		});
+
+	});
+
+
+});
+
+
+
+
+
+//cambia color el fondo al hacer scroll
+
+window.onscroll=(e)=>{
+
+	const scroll =window.scrollY;
+
+	const header =document.querySelector('#navegacion-principal');
+	console.log(scroll);
+	if(scroll > 300){
+		header.classList.add('bg-success');
+	}else{
+		header.classList.remove('bg-success');
+	}
+
+}
+
+
+
+
+
+//cuenta atras 
+$(document).ready(function(){
+	$('#fecha').countdown('2019/08/26', function(evento){
+		$(this).html(evento.strftime(' <span> %D </span> Dias  <span> %H </span>  Horas <span> %M </span> Minutos <span> %S </span> Segundos'));
+
+	});
+
+
+});
